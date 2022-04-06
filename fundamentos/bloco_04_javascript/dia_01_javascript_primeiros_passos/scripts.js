f3f9d4f939c9fbe77915b9cc17e627f9737fe879
Sally Diament <sally.barzilai@gmail.com>
2022-04-06 11:34:58 -0300

    incluindo para fixar do switch e case

--- fundamentos/bloco_04_javascript/dia_01_javascript_primeiros_passos/scripts.js
+++ fundamentos/bloco_04_javascript/dia_01_javascript_primeiros_passos/scripts.js
@@ -4,29 +4,29 @@ let birthYear = 1987;
 birthYear = 2030;
 console.log(myName, birthCity, birthYear);
 
-let patientId = 50 ;
+let patientId = 50;
 let isEnrolled = true;
 const patientInfo = {
     firstName: "Ana",
     lastName: "Santos",
 };
-const patientEmail = "patient@gmail" ;
+const patientEmail = "patient@gmail";
 
 console.log(typeof patientId, isEnrolled, patientInfo, patientEmail);
 console.log(typeof patientAge);
 
 const base = 5;
 const height = 8;
-const area = base*height;
+const area = base * height;
 console.log(area);
-const perimeter = (base+height)*2
+const perimeter = (base + height) * 2
 console.log(perimeter);
 
 const nota = 20;
-if (nota >= 80){
+if (nota >= 80) {
     console.log("Parabéns, vc foi aprovado");
 }
-else if (nota<80 && nota>=60) {
+else if (nota < 80 && nota >= 60) {
     console.log("Vc está na lista de espera");
 }
 else {
@@ -35,34 +35,50 @@ else {
 
 const currentHour = 11;
 let message;
-if (currentHour>=22){
-    message="hora de dormir";
+if (currentHour >= 22) {
+    message = "hora de dormir";
     console.log(message);
 }
-else if (currentHour>=18 && currentHour<22) {
-    message="vamos jantar";
+else if (currentHour >= 18 && currentHour < 22) {
+    message = "vamos jantar";
     console.log(message);
 }
-else if (currentHour >=14 && currentHour<18){
+else if (currentHour >= 14 && currentHour < 18) {
     message = "bolo da tarde";
     console.log(message);
 }
-else if (currentHour >=11 && currentHour<14){
+else if (currentHour >= 11 && currentHour < 14) {
     message = "almoço";
     console.log(message);
 }
-else if (currentHour >=4 && currentHour<11){
+else if (currentHour >= 4 && currentHour < 11) {
     message = "cheiro de café";
     console.log(message);
 }
 
 let weekDay = "domingo";
-if (weekDay === "segunda-feira" || weekDay === "terça-feira" || weekDay === "quarta-feira"){
+if (weekDay === "segunda-feira" || weekDay === "terça-feira" || weekDay === "quarta-feira") {
     console.log("Mais um dia de aprendizado");
 }
 else {
     console.log("dia de descanso");
 }
 
-console.log((2+2)===4);
-console.log(!(2+2)===4);
+console.log((2 + 2) === 4);
+console.log(!(2 + 2) === 4);
+
+let statusCandidato;
+statusCandidato = "aprovada"
+switch (statusCandidato) {
+    case "aprovada":
+        console.log("aprovada");
+        break;
+    case "lista":
+        console.log("lista");
+        break;
+    case "reprovada":
+        console.log("reprovada");
+        break;
+    default:
+        console.log("não se aplica");
+}
