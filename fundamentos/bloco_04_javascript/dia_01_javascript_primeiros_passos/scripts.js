4dcb4eab981056ec9e9c8bed02f75352f1075785
Sally Diament <sally.barzilai@gmail.com>
2022-04-06 11:00:18 -0300

    Adicinonando o segundo para fixar tipos primitivos e operações aritméticas'

--- fundamentos/bloco_04_javascript/dia_01_javascript_primeiros_passos/scripts.js
+++ fundamentos/bloco_04_javascript/dia_01_javascript_primeiros_passos/scripts.js
@@ -3,3 +3,21 @@ const birthCity = "São Paulo";
 let birthYear = 1987;
 birthYear = 2030;
 console.log(myName, birthCity, birthYear);
+
+let patientId = 50 ;
+let isEnrolled = true;
+const patientInfo = {
+    firstName: "Ana",
+    lastName: "Santos",
+};
+const patientEmail = "patient@gmail" ;
+
+console.log(typeof patientId, isEnrolled, patientInfo, patientEmail);
+console.log(typeof patientAge);
+
+const base = 5;
+const height = 8;
+const area = base*height;
+console.log(area);
+const perimeter = (base+height)*2
+console.log(perimeter);
