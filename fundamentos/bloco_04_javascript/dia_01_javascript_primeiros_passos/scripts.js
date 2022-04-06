391277eaa7181c34abe4c3bc9260355fe9ca0c76
Sally Diament <sally.barzilai@gmail.com>
2022-04-06 11:06:43 -0300

    incluindo para fixar de condições if e else

--- fundamentos/bloco_04_javascript/dia_01_javascript_primeiros_passos/scripts.js
+++ fundamentos/bloco_04_javascript/dia_01_javascript_primeiros_passos/scripts.js
@@ -21,3 +21,15 @@ const area = base*height;
 console.log(area);
 const perimeter = (base+height)*2
 console.log(perimeter);
+
+const nota = 20;
+if (nota >= 80){
+    console.log("Parabéns, vc foi aprovado");
+}
+else if (nota<80 && nota>=60) {
+    console.log("Vc está na lista de espera");
+}
+else {
+    console.log("Reprovado");
+}
+
