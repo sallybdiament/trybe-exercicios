335c8b1ff750dc3da40f4b3465b54fa39e39ee4c
Sally Diament <sally.barzilai@gmail.com>
2022-04-11 13:34:07 -0300

    adicionando

--- fundamentos/bloco_04_introducao_a_javascript_e_logica_de_programacao/dia_03_logica_de_programacao_e_algoritmos/exercicios_4_3.js
+++ fundamentos/bloco_04_introducao_a_javascript_e_logica_de_programacao/dia_03_logica_de_programacao_e_algoritmos/exercicios_4_3.js
@@ -22,3 +22,34 @@ backwardWord += word[indexWord]
 }
 console.log(backwardWord);
 
+// exercicio 3./
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+let palavraMaior = array[0];
+let palavraMenor = array[0];
+
+for (indexArray=0 ; indexArray<array.length ; indexArray+=1){
+    if (palavraMaior.length<array[indexArray].length){palavraMaior=array[indexArray]}
+    }
+console.log(palavraMaior);
+
+for (indexArray=0 ; indexArray<array.length ; indexArray+=1){
+    if (array[indexArray].length<palavraMenor.length){palavraMenor=array[indexArray]}
+    }
+console.log(palavraMenor);
+
+
+//exercício 4./
+let maiorNumPrimo = 0;
+
+for (let numAtual =0 ; numAtual<=50; numAtual+=1){
+    let ehPrimo =true;
+    for (let numDivisor = 2; numDivisor<numAtual; numDivisor+=1){
+        if (numAtual%numDivisor===0){
+            ehPrimo =false;
+        }
+    }
+    if (ehPrimo===true){
+        maiorNumPrimo = numAtual;
+    }
+}
+console.log(maiorNumPrimo);
