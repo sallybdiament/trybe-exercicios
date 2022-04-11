05968d0dcf88793a6596a1839a141e76aa2d58a4
Sally Diament <sally.barzilai@gmail.com>
2022-04-11 16:31:07 -0300

    incluindo os exercícios da Parte I - Objetos e For/In de 4.4

--- fundamentos/bloco_04_introducao_javascript_e_logica_de_programacao/dia_04_javascript_objetos_e_funcoes/exercicios_4_4.js
+++ fundamentos/bloco_04_introducao_javascript_e_logica_de_programacao/dia_04_javascript_objetos_e_funcoes/exercicios_4_4.js
@@ -95,3 +95,74 @@ function triangle (lado1, lado2, lado3){
   
 }
 
+//exercicios Parte I/ 1./
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+};
+
+console.log('Bem vinda, '+ info.personagem);
+
+//exercicios Parte I/ 2./
+info['recorrente'] = 'sim';
+
+console.log(info);
+
+//exercicios Parte I/ 3./
+for (let key in info){
+  console.log(key);
+}
+
+//exercicios Parte I/ 4./
+for (let key in info){
+  console.log(info[key]);
+}
+
+//exercicios Parte I/ 5./
+let info2 = {
+  personagem: 'Tio Patinhas',
+  origem: 'Christmas on Bear Mountain, Dell s Four Color Comics #178',
+  nota: 'O último MacPatinhas',
+  recorrente: 'sim',
+};
+
+for (let key in info){
+  if (key === 'recorrente' && info[key] === 'sim' && info2[key] === 'sim'){
+    console.log('Ambos recorrente');
+  }
+  else {
+    console.log(info[key]+ ' e ' + info2[key]);
+  }
+}
+
+//exercicios Parte I/ 6./
+let leitor = {
+  nome: 'Julia',
+  sobrenome: 'Pessoa',
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: 'O Pior Dia de Todos',
+      autor: 'Daniela Kopsch',
+      editora: 'Tordesilhas',
+    },
+  ],
+};
+
+console.log(leitor.livrosFavoritos[0].titulo);
+
+console.log('O livro favorito de ' + leitor.nome + ' ' +leitor.sobrenome+ ' se chama '+ leitor['livrosFavoritos'][0]['titulo'] +'.');
+
+//exercicios Parte I/ 7./
+leitor['livrosFavoritos'].push ({
+  titulo: 'Harry Potter',
+  autor: 'JK Rowling',
+  editor: 'Rocco',
+},
+);
+
+console.log(leitor);
+
+//exercicios Parte I/ 8./
+console.log(leitor.nome + ' tem ' + leitor.livrosFavoritos.length + " livros favoritos.");
