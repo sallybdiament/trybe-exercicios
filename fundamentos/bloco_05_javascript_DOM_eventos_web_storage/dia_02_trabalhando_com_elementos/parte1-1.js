5a27fb6b5d834274830101b8f90601a4ce2bfbfa
Sally Diament <sally.barzilai@gmail.com>
2022-04-14 16:15:27 -0300

    incluindo o para fixar da parte 2 do 5.2

--- fundamentos/bloco_05_javascript_DOM_eventos_web_storage/dia_02_trabalhando_com_elementos/parte1-1.js
+++ fundamentos/bloco_05_javascript_DOM_eventos_web_storage/dia_02_trabalhando_com_elementos/parte1-1.js
@@ -17,3 +17,20 @@ let atencao = elementoOndeVoceEsta.nextSibling;
 let terceiroFilho =  elementoOndeVoceEsta.nextElementSibling;
 
 let terceiroFilho2 = pai.lastElementChild.previousElementSibling;
+
+let irmao = document.createElement('section');
+irmao.id = 'irmao';
+pai.appendChild(irmao);
+
+let filhoDeOndeEsta = document.createElement('p');
+filhoDeOndeEsta.id = 'filhoDeOndeEsta';
+elementoOndeVoceEsta.appendChild(filhoDeOndeEsta);
+
+let primeiroFilhoDoFilho = document.getElementById('primeiroFilhoDoFilho');
+let filhoDoPrimeiroFilhoDoFilho = document.createElement('section');
+filhoDoPrimeiroFilhoDoFilho.id = 'filhoDoPrimeiroFilhoDoFilho';
+primeiroFilhoDoFilho.appendChild(filhoDoPrimeiroFilhoDoFilho);
+
+const terceiroFilho3 = filhoDoPrimeiroFilhoDoFilho.parentElement.parentElement.nextElementSibling;
+console.log(terceiroFilho3);
+
