89e9bae99aa9e07862a932e44f2de0a1570cf2b8
Sally Diament <sally.barzilai@gmail.com>
2022-05-09 15:50:13 -0300

    incluindo os exercícios da parte 1

---
+++ fundamentos/bloco_07_javascript_es6_e_testes_unitarios/dia_02_javascript_es6_fluxo_de_excecao_e_objetos/conteudo.js
@@ -0,0 +1,126 @@
+// Conteúdo: Throw e Catch:
+// const sumSemThrow = (value1, value2) => value1 + value2;
+// console.log(sumSemThrow(2, 3));
+
+// console.log(sumSemThrow(2, '3')); // resultado: 23
+
+const verifyIsNumber = (value1, value2) => {
+    if (typeof value1 !== 'number' || typeof value2 !== 'number') {
+      throw new Error('Os valores devem ser numéricos');
+    }
+  };
+  
+  const sum = (value1, value2) => {
+    try {
+      verifyIsNumber(value1, value2);
+      return value1 + value2;
+    } catch (error) {
+      throw error.message;
+    }
+  };
+  
+  console.log(sum(2, '3'));
+
+  // Parte Objetos://
+  const customer1 = {
+    firstName: 'Roberto',
+    age: 22,
+    job: 'Teacher',
+  };
+  
+  console.log(customer1);
+  
+  customer1.lastName = 'Faria';
+  console.log(customer1);
+  
+  const customer2 = {
+    firstName: 'Maria',
+    age: 23,
+    job: 'Medic',
+  };
+  
+  console.log(customer2);
+  customer2['lastName'] = 'Silva';
+  console.log(customer2);
+
+  const customer = {
+    firstName: 'Roberto',
+    age: 22,
+    job: 'Teacher',
+  };
+  
+  let newKey = 'lastName';
+  const lastName = 'Ferreira';
+  customer[newKey] = lastName;
+  newKey = 'fullName';
+  const fullName = `${customer.firstName} ${customer.lastName}`;
+  customer[newKey] = fullName;
+  console.log(customer);
+
+  // Object.key://
+  const student1 = {
+    Html: 'Muito Bom',
+    Css: 'Bom',
+    JavaScript: 'Ótimo',
+    SoftSkills: 'Ótimo',
+  };
+  
+  const student2 = {
+    Html: 'Bom',
+    Css: 'Ótimo',
+    JavaScript: 'Ruim',
+    SoftSkills: 'Ótimo',
+    Git: 'Bom', // chave adicionada
+  };
+  
+  const listSkills = (student) => {
+    const arrayOfSkills = Object.keys(student);
+    for (const index in arrayOfSkills) {
+      console.log(`${arrayOfSkills[index]}, Nível: ${student[arrayOfSkills[index]]}`);
+    }
+  };
+  
+  console.log('Estudante 1');
+  listSkills(student1);
+  
+  console.log('Estudante 2');
+  listSkills(student2);
+
+  // Objetc.values://
+  const student = {
+    Html: 'Muito Bom',
+    Css: 'Bom',
+    JavaScript: 'Ótimo',
+    SoftSkill: 'Ótimo',
+  };
+  
+  const listSkillsValuesWithFor = (student) => {
+    const skills = [];
+    for(skill in student) {
+      skills.push(student[skill]);
+    }
+  
+    return skills;
+  };
+  
+  const listSkillsValuesWithValues = (student) => Object.values(student);
+  
+  // Sem Object.values
+  console.log(listSkillsValuesWithFor(student));
+  
+  // Com Object.values
+  console.log(listSkillsValuesWithValues(student));
+
+  //Object.assign://
+  const person = {
+    name: 'Roberto',
+  };
+  
+  const lastName = {
+    lastName: 'Silva',
+  };
+  
+  const clone = Object.assign(person, lastName);
+  
+  console.log(clone); // { name: 'Roberto', lastName: 'Silva' }
+  console.log(person); // { name: 'Roberto', lastName: 'Silva' }
