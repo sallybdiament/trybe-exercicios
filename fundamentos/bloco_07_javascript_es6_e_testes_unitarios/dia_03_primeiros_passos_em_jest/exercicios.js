8f281a6186aaa82d212f90f7d106d3ddb76d2cfb
Sally Diament <sally.barzilai@gmail.com>
2022-05-10 22:58:21 -0300

    incluindo os exercicios 2 e 3

--- fundamentos/bloco_07_javascript_es6_e_testes_unitarios/dia_03_primeiros_passos_em_jest/exercicios.js
+++ fundamentos/bloco_07_javascript_es6_e_testes_unitarios/dia_03_primeiros_passos_em_jest/exercicios.js
@@ -6,4 +6,24 @@ function sum(a, b) {
     return a + b;
   }
 
+  function myRemove(arr, item) {
+    let newArr = [];
+    for (let index = 0; index < arr.length; index += 1) {
+      if (item !== arr[index]) {
+        newArr.push(arr[index]);
+      }
+    }
+    return newArr;
+  }
+  
+  function myFizzBuzz(num) {
+    if (typeof num !== 'number') return false;
+    if (num % 3 === 0 && num % 5 === 0) return 'fizzbuzz';
+    if (num % 3 === 0) return 'fizz';
+    if (num % 5 === 0) return 'buzz';
+    return num;
+  }
+
   module.exports = sum;
+  module.exports = myRemove;
+  module.exports = myFizzBuzz;
--- fundamentos/bloco_07_javascript_es6_e_testes_unitarios/dia_03_primeiros_passos_em_jest/exercicios.test.js
+++ fundamentos/bloco_07_javascript_es6_e_testes_unitarios/dia_03_primeiros_passos_em_jest/exercicios.test.js
@@ -1,16 +1,51 @@
-const sum = require('./exercicios');
+// const sum = require('./exercicios');
 
-describe ('Testes do ex 1 de soma', () => {
+// describe ('Testes do ex 1 de soma', () => {
 
-    test ('Soma de 4 e 5 igual 9', () => {
-        expect(sum(4, 5)).toBe(9);
-    })
+//     test ('Soma de 4 e 5 igual 9', () => {
+//         expect(sum(4, 5)).toBe(9);
+//     })
 
-    test ('Soma de 0 e 0 igual 0', () => {
-        expect(sum(0, 0)).toBe(0);
-    })
+//     test ('Soma de 0 e 0 igual 0', () => {
+//         expect(sum(0, 0)).toBe(0);
+//     })
 
-    test ('Soma de 4 e string "5" igual 9', () => {
-        expect( () => {sum(4, '5')}).toThrow('parameters must be numbers');
-    })
+//     test ('Soma de 4 e string "5" igual 9', () => {
+//         expect( () => {sum(4, '5')}).toThrow('parameters must be numbers');
+//     })
+// })
+
+// const myRemove = require('./exercicios');
+
+// describe ('Testes exercício 2 sobre uma função que copia um array retirando um item do array', () => {
+//     test ('Retira o 3 do array [1, 2, 3, 4] e retorna [1, 2, 4]', () => {
+// expect(myRemove([1, 2, 3, 4], 3)).toStrictEqual([1, 2, 4]);
+//     })
+//     test ('Retira o 3 do array [1, 2, 3, 4] e não retorna [1, 2, 3, 4]', () => {
+//         expect(myRemove([1, 2, 3, 4], 3)).not.toStrictEqual([1, 2, 3, 4]);
+//    })
+//    test ('Não retira nada do array [1, 2, 3, 4] quando o item é 5', () => {
+//     expect(myRemove([1, 2, 3, 4], 5)).toStrictEqual([1, 2, 3, 4]);
+//         })
+
+// })
+
+const myFizzBuzz = require('./exercicios');
+
+describe ('Testes para exercício 3 da função myFizzBuzz', () => {
+test ('Retorna fizzbuzz para 15', () => {
+    expect(myFizzBuzz(15)).toBe('fizzbuzz');
+})
+test ('Retorna fizz para 9', () => {
+    expect(myFizzBuzz(9)).toBe('fizz');
+})
+test ('Retorna buzz para 5', () => {
+    expect(myFizzBuzz(5)).toBe('buzz');
+})
+test ('Retorna 7 para 7', () => {
+    expect(myFizzBuzz(7)).toBe(7);
+})
+test ('Retorna false para "a"', () => {
+    expect(myFizzBuzz('a')).toBeFalsy();
+})
 })
