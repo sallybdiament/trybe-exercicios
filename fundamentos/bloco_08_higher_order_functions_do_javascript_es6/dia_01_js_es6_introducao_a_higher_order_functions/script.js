015b1ecd0453dd440e0a920b1cc69d92a7c349f7
Sally Diament <sally.barzilai@gmail.com>
2022-05-13 20:59:12 -0300

    incluindo exercicios 2 e 3

--- fundamentos/bloco_08_higher_order_functions_do_javascript_es6/dia_01_js_es6_introducao_a_higher_order_functions/script.js
+++ fundamentos/bloco_08_higher_order_functions_do_javascript_es6/dia_01_js_es6_introducao_a_higher_order_functions/script.js
@@ -11,3 +11,35 @@ const doingThings = (callback) => {
 console.log(doingThings(acordando));
 console.log(doingThings(cafe));
 console.log(doingThings(dormir));
+
+// Exercício 1://
+const novaPessoa = (nome) => {
+    const nomeParaEmail = nome.replace(/ /g, '_');
+    const email = `${nomeParaEmail}@trybe.com`;
+    const objeto = {
+        name: nome,
+        email: email.toLowerCase(),
+    };
+    return objeto;
+}
+
+const newEmployees = (callback) => {
+    const employees = {
+        id1: novaPessoa('Pedro Guerra'),
+        id2: novaPessoa('Luiza Drumond'),
+        id3: novaPessoa('Carla Paiva'),
+    }
+    return employees;
+};
+
+console.log(newEmployees(novaPessoa));
+
+// Exercicio 2://
+const checaResultado = (numApostado, numSorteado) => numApostado === numSorteado;
+
+const resultadoLoteria = (numApostado, callback) => {
+    const numSorteado = Math.floor((Math.random()*5)+1);
+    return callback(numApostado, numSorteado) ? 'Lucky day, you won' : 'Try Again';
+};
+
+console.log(resultadoLoteria(2, checaResultado));
