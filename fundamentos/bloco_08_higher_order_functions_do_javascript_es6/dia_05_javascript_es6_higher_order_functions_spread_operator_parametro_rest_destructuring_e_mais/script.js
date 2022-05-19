76dbd9ab12f5ce3c68382b702bb9b729a7e6879c
Sally Diament <sally.barzilai@gmail.com>
2022-05-19 20:00:51 -0300

    incluindo o para fixar

---
+++ fundamentos/bloco_08_higher_order_functions_do_javascript_es6/dia_05_javascript_es6_higher_order_functions_spread_operator_parametro_rest_destructuring_e_mais/script.js
@@ -0,0 +1,128 @@
+//Spread Operator://
+// Faça uma lista com as suas frutas favoritas
+const specialFruit = ['maça', 'pera', 'banana'];
+
+// Faça uma lista de complementos que você gostaria de adicionar
+const additionalItens = ['pitaya', 'figo', 'ameixa'];
+
+const fruitSalad = (fruit, additional) => {
+  return [...specialFruit, ...additionalItens];
+};
+
+console.log(fruitSalad(specialFruit, additionalItens));
+
+// Parametro Rest//
+function quantosParams(...args) {
+    console.log('parâmetros:', args);
+    return `Você passou ${args.length} parâmetros para a função.`;
+  }
+  
+  console.log(quantosParams(0, 1, 2)); // Você passou 3 parâmetros para a função.
+  console.log(quantosParams('string', null, [1, 2, 3], { })); // Você passou 4 parâmetros para a função.
+
+  const sum = (...args) => args.reduce((accumulator, current) => accumulator + current, 0);
+console.log(sum(4, 7, 8, 9, 60)); // 88
+
+//Object Destructuring://
+const product = {
+    name: 'Smart TV Crystal UHD',
+    price: '1899.05',
+    seller: 'Casas de Minas',
+  };
+  
+  const printProductDetails = ({ name, price, seller }) => {
+    console.log(`Promoção! ${name} por apenas ${price} é só aqui: ${seller}`);
+  };
+  
+  printProductDetails(product); // Promoção! Smart TV Crystal UHD por apenas 1899.05 é só aqui: Casas de Minas
+
+
+const user = {
+    name: 'Maria',
+    age: 21,
+    nationality: 'Brazilian',
+  };
+  
+  const jobInfos = {
+    profession: 'Software engineer',
+    squad: 'Rocket Landing Logic',
+    squadInitials: 'RLL',
+  };
+
+  const novoObjeto = {...user, ...jobInfos};
+  console.log(novoObjeto);
+  
+  const printDetailsObject = ({name, age, nationality, profession, squad, squadInitials}) => {
+    console.log(`Hi, my name is ${name}, I'm ${age} years old and I'm ${nationality}. I work as a ${profession} and my squad is ${squadInitials}-${squad}.`)};
+
+    printDetailsObject(novoObjeto);
+
+    // Array Destructuring://
+    const saudacoes = ['Olá', (saudacao) => console.log(saudacao)];
+
+saudacoes[1](saudacoes[0]); // Olá
+
+const [primeiro, segundo] = saudacoes;
+console.log(primeiro);
+
+let comida = 'gato';
+let animal = 'água';
+let bebida = 'arroz';
+
+console.log(comida, animal, bebida); // arroz gato água
+
+// Utilizando array destructuring, faça com que os valores apareçam nas variáveis correspondentes ao seu verdadeiro tipo
+[comida, animal, bebida] = [bebida, comida, animal]; // Criando o array e o desestruturando
+console.log(comida, animal, bebida); // arroz gato água
+
+let numerosPares = [1, 3, 5, 6, 8, 10, 12];
+
+console.log(numerosPares); // [6, 8, 10, 12];
+
+// Utilize array destructuring para produzir o resultado esperado pelo console.log abaixo
+[,,, ...numerosPares] = numerosPares;
+console.log(numerosPares);
+
+//Default Destructuring://
+const person = {
+    name: 'João',
+    lastName: 'Jr',
+    age: 34,
+  };
+  
+  const { nationality } = person;
+  console.log(nationality);
+
+//   const getNationality = ({ firstName, nationality = 'Brazilian }) => `${firstName} is ${nationality}`;
+
+// const person2 = {
+//   firstName: 'João',
+//   lastName: 'Jr II',
+// };
+
+// const otherPerson = {
+//   firstName: 'Ivan',
+//   lastName: 'Ivanovich',
+//   nationality: 'Russian',
+// };
+
+// const {firstName, lastName, nationality2 = 'Brazilain'} = person2;
+
+// console.log(getNationality(otherPerson)); // Ivan is Russian
+// console.log(getNationality(person2));
+
+const getNationality = ({ firstName, nationality = "Brazilian" }) => `${firstName} is ${nationality}`;
+
+const person2 = {
+  firstName: 'João',
+  lastName: 'Jr II',
+};
+
+const otherPerson = {
+  firstName: 'Ivan',
+  lastName: 'Ivanovich',
+  nationality: 'Russian',
+};
+
+console.log(getNationality(otherPerson)); // Ivan is Russian
+console.log(getNationality(person2)); // João is Brazilian
