076dfffa73008444cf8be92ba6c8ea17cfe832d4
Sally Diament <sally.barzilai@gmail.com>
2022-06-01 15:08:12 -0300

    exemplo do conteudo fetch

---
+++ front-end/bloco_09_javascript_e_testes_assíncronos/dia_02_javascript_assíncrono_fetch_api_e_async_await/script.js
@@ -0,0 +1,17 @@
+const fetch = require('node-fetch');
+
+const fetchJoke = () => {
+//   const url = 'https://api.chucknorris.io/jokes/random?category=dev';
+// Note que para forçar o erro retiramos o https:// do início da url.
+const url = 'api.chucknorris.io/jokes/random?category=dev';
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => console.log(data.value))
+    .catch((error) => console.log(`Algo deu errado :( \n${error}`));
+}
+
+fetchJoke();
+
+// Algo deu errado :(
+// TypeError: Only absolute URLs are supported
