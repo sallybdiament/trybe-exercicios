ea56fdf9cae5278ff4ce6bf3cd4de7ac8f5b1a3c
Sally Diament <sally.barzilai@gmail.com>
2022-06-13 15:01:33 -0300

    concluindo os exercicios

--- front-end/bloco_10_introducao_a_react/dia_01_hello_world_no_react/exercicios_10.1/src/App.js
+++ front-end/bloco_10_introducao_a_react/dia_01_hello_world_no_react/exercicios_10.1/src/App.js
@@ -1,17 +1,38 @@
 import './App.css';
+import Header from './Header';
+import React from 'react';
+import Content  from './Content';
+import Footer from './Footer';
 
-const Task = (value) => {
-  return (
-    <li>{value}</li>
-  );
-}
+// const Task = (value) => {
+//   return (
+//     <li>{value}</li>
+//   );
+// }
 
-function App() {
-  return (
-    <div className="App">
-     { Task ('teste')};
-    </div>
-  );
+// const tarefas = ['levar na escola', 'tomar café', 'fazer os exercícios da trybe' ];
+
+// function App() {
+//   return (
+//   <>
+//       <Header />
+//     <div className="App">
+//      { tarefas.map((tarefa) => Task(tarefa)) };
+//     </div>
+//     </>
+//   );
+// }
+
+class App extends React.Component {
+  render () {
+    return (
+    <>
+      <Header />
+      <Content />
+      <Footer />
+    </>
+    )
+  }
 }
 
 export default App;
