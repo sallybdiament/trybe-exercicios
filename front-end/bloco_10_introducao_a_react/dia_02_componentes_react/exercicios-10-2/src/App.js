5fc47010188fd71e856a3371d6997e40b335cfe1
Sally Diament <sally.barzilai@gmail.com>
2022-06-14 19:38:48 -0300

    incluindo o para fixar 2

--- front-end/bloco_10_introducao_a_react/dia_02_componentes_react/exercicios-10-2/src/App.js
+++ front-end/bloco_10_introducao_a_react/dia_02_componentes_react/exercicios-10-2/src/App.js
@@ -1,11 +1,37 @@
 import './App.css';
-import Image from './image';
+import Image from './Image';
 import React from 'react';
+import Order from './Order';
 
 class App extends React.Component {
   render() {
-  return (
-    <Image source='https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg' alternativeText='Cute cat staring' />
+    const headphone = {
+      id: 102,
+      user: "cena@gmail.com",
+      product: "Razer Headphone",
+      price: {
+        value: 99.99,
+        currency: "dollars"
+      }
+    };
+
+    const energyDrink = {
+      id: 77,
+      user: "cena@gmail.com",
+      product: "Monster 500mL",
+      price: {
+        value: 9.99,
+        currency: "dollars"
+      }
+    };
+
+    return (
+      <div className="App">
+        <h1> Orders recently created </h1>
+         <Order order={energyDrink} />
+         <Order order={headphone} />
+         <Image source='https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg' alternativeText='Cute cat staring' />
+      </div>  
   );
 }
 }
