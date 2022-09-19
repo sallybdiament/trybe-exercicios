aedb4f87dcca9849e10ae64325224b53eecfdbb7
Sally Diament <sally.barzilai@gmail.com>
2022-09-18 22:13:44 -0300

    incluindo o primeiro teste e a api funcionadnado

--- back-end/bloco_22_introducao_ao_desenvolvimento_web_com_node_js/dia_03_node_js_testes_de_integracao/cacao-trybe/tests/integration/chocolates.test.js
+++ back-end/bloco_22_introducao_ao_desenvolvimento_web_com_node_js/dia_03_node_js_testes_de_integracao/cacao-trybe/tests/integration/chocolates.test.js
@@ -1,10 +1,21 @@
 const chai = require('chai');
-
 const { expect } = chai;
+const chaiHttp = require('chai-http');
+const app = require('../../src/app');
+
+chai.use(chaiHttp);
 
 describe('Usando o método GET em /chocolates', function () {
     it('Retorna a lista completa de chocolates!', async function () {
-        response = await minhaRequisicao();
+        response = await chai.request(app).get('/chocolates');
         expect(response.status).to.be.equal(200);
+
+        const output = [
+            { id: 1, name: 'Mint Intense', brandId: 1 },
+            { id: 2, name: 'White Coconut', brandId: 1 },
+            { id: 3, name: 'Mon Chéri', brandId: 2 },
+            { id: 4, name: 'Mounds', brandId: 3 },
+          ];
+          expect(response.body.chocolates).to.deep.equal(output);
     });
 });
