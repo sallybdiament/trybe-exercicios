6f3206f54840fb968d81e76b70dc1047841b028f
Sally Diament <sally.barzilai@gmail.com>
2022-11-21 10:28:38 -0300

    incluindo o exercício 4 classe Subject

--- back-end/bloco_27_programacao_orientada_a_objeto_poo_e_solid/dia_02_heranca_e_interfaces/index.ts
+++ back-end/bloco_27_programacao_orientada_a_objeto_poo_e_solid/dia_02_heranca_e_interfaces/index.ts
@@ -1,12 +1,15 @@
 import Person from './Person';
 import Student from './Student';
+import Employee from './IEmployee';
 
+// Exercício 1 (classe Person):
 const maria = new Person('Maria da Consolação', new Date('1980/01/25'));
 const luiza = new Person('Luiza Andrade', new Date('2005/10/02'));
 
 console.log(maria);
 console.log(luiza);
 
+// Exercício 2 (classe Student)
 const carolina = new Student('Carolina da Silva', new Date('2005/03/17'));
 const lucas = new Student('Lucas Peixoto Salgueiro', new Date('2006/07/19'));
 const jessica = new Student('Jéssica Bianca Nunes', new Date('2004/06/06'));
@@ -23,3 +26,19 @@ console.log(lucas);
 console.log(jessica);
 console.log(tamires);
 console.log(fernando);
+
+// Exercício 3 (interface Employee):
+const testInterfaceEmployee: Employee = {
+  registration: 'FNC1234567891011',
+  salary: 1200.00,
+  admissionDate: new Date(),
+
+  generateRegistration(): string {
+    // eslint-disable-next-line max-len
+    const randomStr = String(Date.now() * (Math.random() + 1)).replace(/\W/g, '');
+
+    return `FNC${randomStr}`;
+  },
+};
+
+console.log(testInterfaceEmployee);
